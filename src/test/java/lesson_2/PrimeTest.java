737d29b2160014f1a3bc23c737aa704f03565086
unknown <emilia.deklic@gmail.com>
2015-07-15 21:12:58 +0300

    PrimeTest

---
+++ src/test/java/lesson_2/PrimeTest.java
@@ -0,0 +1,25 @@
+package lesson_2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by selenium on 13.07.2015.
+ */
+public class PrimeTest {
+
+    @Test
+    public void isPrimeNumberTest (){
+
+        Assert.assertTrue(isPrime(8));
+    }
+
+    boolean isPrime(int N) {
+        if (N < 2) return false;
+        for (int i = 2; i*i <= N; i++)
+            if (N % i == 0) return false;
+        return true;
+    }
+
+
+}
