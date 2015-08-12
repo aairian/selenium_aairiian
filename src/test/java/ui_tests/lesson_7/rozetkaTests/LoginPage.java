e9f8dda88486e57e0de784f7e5baf93e62e45cf7
aairiian <emilia.deklic@gmail.com>
2015-08-12 03:07:16 +0300

    tryingWriteTestAsShould

---
+++ src/test/java/ui_tests/lesson_7/rozetkaTests/LoginPage.java
@@ -0,0 +1,17 @@
+package ui_tests.lesson_7.rozetkaTests;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by anny on 12.08.15.
+ */
+public class LoginPage {
+    private WebDriver driver;
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+
+}
