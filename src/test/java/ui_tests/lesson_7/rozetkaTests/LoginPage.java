2638d25bf26880d940115471128021f7ad342a21
aairiian <emilia.deklic@gmail.com>
2015-08-12 21:39:25 +0300

    //

--- src/test/java/ui_tests/lesson_7/rozetkaTests/LoginPage.java
+++ src/test/java/ui_tests/lesson_7/rozetkaTests/LoginPage.java
@@ -1,6 +1,12 @@
 package ui_tests.lesson_7.rozetkaTests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.Set;
+
+import static org.openqa.selenium.By.xpath;
 
 /**
  * Created by anny on 12.08.15.
@@ -11,7 +17,24 @@ public class LoginPage {
         this.driver = driver;
     }
 
-
-
-
+    public VKLoginPage openVKLoginWindow() {
+//        String parentWindowId = driver.getWindowHandle();
+        WebElement openPopUpWindowButton = driver.findElement(By.xpath("//a[@class='btn-link-i' and text()='ВКонтакте']"));
+        openPopUpWindowButton.click();
+        //add waiting
+        Set<String> allWindows = driver.getWindowHandles();
+        if (!allWindows.isEmpty()) {
+            for (String windowId : allWindows) {
+                if (driver.switchTo().window(windowId).getTitle().equals("ВКонтакте")) ;
+                driver.close();
+            }
+        }
+        return new VKLoginPage(driver);
+    }
 }
+
+
+
+
+
+
