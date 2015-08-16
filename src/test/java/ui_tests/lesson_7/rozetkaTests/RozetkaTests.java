a902e4487ae658f2aa7ab826f45eed0f7fa1f331
aairiian <emilia.deklic@gmail.com>
2015-08-16 23:29:26 +0300

    finishedVer1.0

--- src/test/java/ui_tests/lesson_7/rozetkaTests/RozetkaTests.java
+++ src/test/java/ui_tests/lesson_7/rozetkaTests/RozetkaTests.java
@@ -2,20 +2,24 @@ package ui_tests.lesson_7.rozetkaTests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 /**
  * Created by anny on 09.08.15.
  */
 public class RozetkaTests {
     private WebDriver driver;
-//    private String login;
-//    private String password;
+    private String login = "funic89@gmail.com";
+    private String password = "*******";
+    private String userName = "Эмилия Деклик";
+    private String personalDataText = "Личные данные";
 
     @BeforeTest
     public void setUp (){
@@ -25,21 +29,21 @@ public class RozetkaTests {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         assertEquals("http://rozetka.com.ua/", driver.getCurrentUrl());    }
 
-//    @AfterTest
-//    public void tearDown(){
-//        driver.close();
-//    }
+    @AfterTest
+    public void tearDown(){
+        driver.close();
+    }
+
 
     @Test
     public void loginWithVKAccountTest () throws Exception {
         HomePage home = new HomePage(driver);
         LoginPage loginPage = home.openLoginPopWindow();
         VKLoginPage vkLoginPage = loginPage.openVKLoginWindow();
-//        vkLoginPage.entranceWithValidCredentials(login, password);
-//        assertTrue(home.findUserName.getText().contains(UserName));
-//        assertTrue(home.openUserSettings.getText().contains(UserName));
-
-
+        HomePage user = vkLoginPage.entranceWithValidCredentials(login, password);
+        assertTrue(user.findUserName().getText().contains(userName));
+        PersonalDataPage myPersonalData = user.openPersonalData();
+        assertTrue(myPersonalData.findTitle().getText().contains(personalDataText));
 
     }
 
