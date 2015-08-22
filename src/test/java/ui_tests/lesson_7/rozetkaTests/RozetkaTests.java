0e42b2a48b0fe2f8d41383e7d582873b4569a9a9
aairiian <emilia.deklic@gmail.com>
2015-08-22 10:54:45 +0300

    startingNewTest

--- src/test/java/ui_tests/lesson_7/rozetkaTests/RozetkaTests.java
+++ src/test/java/ui_tests/lesson_7/rozetkaTests/RozetkaTests.java
@@ -5,16 +5,25 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.PersonalDataPage;
+import pages.VKLoginPage;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 /**
  * Created by anny on 09.08.15.
  */
 public class RozetkaTests {
     private WebDriver driver;
+    private String login = "funic89@gmail.com";
+    private String password = "hayriyan89";
+    private String userName = "Эмилия Деклик";
+    private String personalDataText = "Личные данные";
 
     @BeforeTest
     public void setUp (){
@@ -29,16 +38,21 @@ public class RozetkaTests {
         driver.close();
     }
 
+
     @Test
-    public void loginWithVKAccountTest (){
+    public void loginWithVKAccountTest () throws Exception {
         HomePage home = new HomePage(driver);
-        LoginPage vkButton = home.openLoginPopWindow();
-
-//                enterToInternetShopPopWindow.click(VKButton);
-//        loginAsVKUser.sendKeys(login, password);
-
+        LoginPage loginPage = home.openLoginPopWindow();
+        VKLoginPage vkLoginPage = loginPage.openVKLoginWindow();
+        HomePage user = vkLoginPage.entranceWithValidCredentials(login, password);
+        assertTrue(user.findUserName().getText().contains(userName));
+        PersonalDataPage myPersonalData = user.openPersonalData();
+        assertTrue(myPersonalData.findTitle().getText().contains(personalDataText));
 
     }
+
+//    @Test
+//    public void finalTask
 
 
 }
