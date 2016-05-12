fdff20d37ff439aec93472d098f12d59b2a62c55
aairiian <emilia.deklic@gmail.com>
2016-05-12 23:32:44 +0300

    first stepDefs of login page

--- src/test/java/cucumber/features/logIn.feature
+++ src/test/java/cucumber/features/logIn.feature
@@ -3,9 +3,10 @@ Feature: Login Action
   Scenario: Successful Login with Valid Credentials
     Given User is on Home Page
     When User Navigate to LogIn Page
-#    And User enters UserName and Password
-#    Then Message displayed Login Successfully
+    And User enters UserName and Password
+    Then Message displayed Login Successfully
 
   Scenario: Successful LogOut
+    Given
     When User LogOut from the Application
     Then Message displayed LogOut Successfully
--- src/test/java/cucumber/stepOfDef/LogInStepDefs.java
+++ src/test/java/cucumber/stepOfDef/LogInStepDefs.java
@@ -1,27 +1,30 @@
 package cucumber.stepOfDef;
 
+import cucumber.annotation.Before;
 import cucumber.annotation.en.Given;
 import cucumber.annotation.en.When;
 import cucumber.runtime.PendingException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
- * Created by anny on 14.02.16.
+ * Created by anny on 14.03.16.
  */
-public class MyStepdefs {
+public class LogInStepDefs {
     public WebDriver driver;
-//    public MyStepdefs (WebDriver driver) {this.driver = driver;}
+//    public LogInStepDefs (WebDriver driver) {this.driver = driver;}
+
+    @Before
+    public void setUP (){
+        driver = new FirefoxDriver();
+    }
 
     @Given("^User is on Home Page$")
-    public void User_is_on_Home_Page() throws Throwable {
-        System.setProperty("webdriver.chrome.driver",
-                "/home/anny/IdeaProjects/main/src/test/resources/drivers/chromedriver");
-        WebDriver driver = new ChromeDriver();
+    public void User_is_on_Home_Page(String homePage)  {
         driver.get("http://www.store.demoqa.com");
+        driver.getCurrentUrl().equals(homePage);
         // Express the Regexp above with the code you wish you had
-        throw new PendingException();
     }
 
     @When("^User Navigate to LogIn Page$")
